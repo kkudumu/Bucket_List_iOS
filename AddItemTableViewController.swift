9a61435ca7e5f8a783a4b79bf84d58962e261a98
Kio <kkudumu@gmail.com>
2017-11-06 18:11:25 -0800

    Finished Bucket List

---
+++ AddItemTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddItemTableViewController.swift
+//  Bucket List 2
+//
+//  Created by Kioja Kudumu on 11/3/17.
+//  Copyright © 2017 Kioja Kudumu. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+    
+    weak var delegate: AddItemTableViewControllerDelegate?
+    var item: String?
+    var indexPath: NSIndexPath?
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        delegate?.cancelButtonPressed(by: self)
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        let text = itemTextField.text!
+        delegate?.itemSaved(by: self, with: text, at: indexPath)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        itemTextField.text = item
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ AddItemTableViewControllerDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AddItemTableViewControllerDelegate.swift
+//  Bucket List 2
+//
+//  Created by Kioja Kudumu on 11/6/17.
+//  Copyright © 2017 Kioja Kudumu. All rights reserved.
+//
+
+import Foundation
+
+protocol AddItemTableViewControllerDelegate: class {
+    func itemSaved(by controller: AddItemTableViewController, with text: String, at indexPath: NSIndexPath?)
+    func cancelButtonPressed(by controller: AddItemTableViewController)
+}
